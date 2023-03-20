3075e1eb52e35dfb76699abffe03287b47d9be67
Brahim EL Mahkour <Brahim EL Mahkour>
2023-03-20 09:13:47 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,10 +2,9 @@
 
 
 /**
- * main -prints the size of various types on the computer it is compiled and run on
- *
+ * main -prints the size of various types using sizeof
  * Return: 0
- */
+*/
 
 int main(void)
 {
